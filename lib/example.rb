da44e395b0292048278a63d6c732b426ca1ead00
Jeff Swisher <jefftswisher@gmail.com>
2019-10-07 17:04:09 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,5 +1,6 @@
 # Use this space to code along with the readme
 
+=begin
 phrase = "Hello World!"
 puts phrase
 puts phrase
@@ -11,3 +12,12 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+=end 
+
+def say_ruby_ten_times
+  phrase = "Hello World"
+    10.times do puts phrase
+  end
+end
+
+say_ruby_ten_times
